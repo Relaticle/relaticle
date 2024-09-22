72c4c457da3478537aa4b109dff3b4b0d9963dad
manukminasyan <manuk.minasyan1@gmail.com>
2024-09-22 17:18:56 +0400

    Tenant Aware package

--- app/Filament/Pages/SettingsPage.php
+++ app/Filament/Pages/SettingsPage.php
@@ -2,29 +2,15 @@
 
 namespace App\Filament\Pages;
 
-use Filament\Facades\Filament;
 use Filament\Navigation\NavigationItem;
-use Filament\Pages\Tenancy\EditTenantProfile;
+use Filament\Pages\Page;
 use ManukMinasyan\FilamentCustomField\Filament\Resources\CustomFieldResource;
 use ManukMinasyan\FilamentCustomField\Services\EntityTypeOptionsService;
 
-class EditTeam extends EditTenantProfile
+class SettingsPage extends Page
 {
-    protected static string $view = 'filament.pages.edit-team';
+    protected static bool $shouldRegisterNavigation = false;
 
-    protected static ?int $navigationSort = 2;
-
-    public static function getLabel(): string
-    {
-        return 'Team Settings';
-    }
-
-    protected function getViewData(): array
-    {
-        return [
-            'team' => Filament::getTenant(),
-        ];
-    }
 
     public function getSubNavigation(): array
     {
