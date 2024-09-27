8a16c2d1e2a6064279f53eee217db775fce44ffa
manukminasyan <manuk.minasyan1@gmail.com>
2024-09-27 12:09:53 +0400

    Laravel horizon install

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,5 +4,6 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AppPanelProvider::class,
     App\Providers\FortifyServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
 ];
