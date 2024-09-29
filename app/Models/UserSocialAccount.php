bf0f9d144e124c6ce1a5536d3f5723881c3258d2
manukminasyan <manuk.minasyan1@gmail.com>
2024-09-29 23:31:50 +0400

    UserSocialAccount factory and user releationshiop

--- app/Models/UserSocialAccount.php
+++ app/Models/UserSocialAccount.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserSocialAccount extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
