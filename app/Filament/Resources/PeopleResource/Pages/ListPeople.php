9d8db0947c783fcfcab4578b3a6a2d25e86a49ae
manukminasyan <manuk.minasyan1@gmail.com>
2024-12-28 19:09:30 +0400

    Create custom fields for people

--- app/Filament/Resources/PeopleResource/Pages/ListPeople.php
+++ app/Filament/Resources/PeopleResource/Pages/ListPeople.php
@@ -5,9 +5,12 @@ namespace App\Filament\Resources\PeopleResource\Pages;
 use App\Filament\Resources\PeopleResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Relaticle\CustomFields\Filament\Tables\Concerns\InteractsWithCustomFields;
 
 class ListPeople extends ListRecords
 {
+    use InteractsWithCustomFields;
+
     protected static string $resource = PeopleResource::class;
 
     protected function getHeaderActions(): array
