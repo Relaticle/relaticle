c9f3c0ad42c66c7741a5965b11077919203aacec
manukminasyan <manuk.minasyan1@gmail.com>
2025-02-02 16:53:23 +0400

    Add custom fields support to Note resource and model

--- app/Filament/Resources/NoteResource/Pages/ManageNotes.php
+++ app/Filament/Resources/NoteResource/Pages/ManageNotes.php
@@ -5,9 +5,12 @@ namespace App\Filament\Resources\NoteResource\Pages;
 use App\Filament\Resources\NoteResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ManageRecords;
+use Relaticle\CustomFields\Filament\Tables\Concerns\InteractsWithCustomFields;
 
 class ManageNotes extends ManageRecords
 {
+    use InteractsWithCustomFields;
+
     protected static string $resource = NoteResource::class;
 
     protected function getHeaderActions(): array
