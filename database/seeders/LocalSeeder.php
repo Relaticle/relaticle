3f29b7f64e8612f65c85aa012bb1cc83f1a702d0
manukminasyan <manuk.minasyan1@gmail.com>
2025-03-17 01:36:07 +0400

    Update LocalSeeder to include new custom field options and increase Opportunity count

--- database/seeders/LocalSeeder.php
+++ database/seeders/LocalSeeder.php
@@ -12,6 +12,7 @@ use Filament\Facades\Filament;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Auth;
 use Relaticle\CustomFields\Models\CustomField;
+use Relaticle\CustomFields\Models\CustomFieldOption;
 
 final class LocalSeeder extends Seeder
 {
@@ -32,19 +33,25 @@ final class LocalSeeder extends Seeder
         People::factory()->for($user->personalTeam(), 'team')->count(500)->create();
 
         $customFields = CustomField::query()
-            ->whereIn('code', ['icp', 'domain_name'])
+            ->whereIn('code', ['icp', 'stage', 'domain_name'])
             ->get()
             ->keyBy('code');
+
 
         Company::factory()
             ->for($user->personalTeam(), 'team')
             ->count(50)
             ->afterCreating(function (Company $company) use ($customFields): void {
-                $company->saveCustomFieldValue($customFields->get('domain_name'), 'https://'.fake()->domainName());
+                $company->saveCustomFieldValue($customFields->get('domain_name'), 'https://' . fake()->domainName());
                 $company->saveCustomFieldValue($customFields->get('icp'), fake()->boolean(70));
             })
             ->create();
 
-        Opportunity::factory()->for($user->personalTeam(), 'team')->count(100)->create();
+        Opportunity::factory()->for($user->personalTeam(), 'team')
+            ->count(150)
+            ->afterCreating(function (Opportunity $opportunity) use ($customFields): void {
+                $opportunity->saveCustomFieldValue($customFields->get('stage'), $customFields->get('stage')->options->random()->id);
+            })
+            ->create();
     }
 }
