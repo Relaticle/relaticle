fc7503a225a1ba3a42f05a4d24b7739d55feedeb
manukminasyan <manuk.minasyan1@gmail.com>
2025-03-16 23:32:16 +0400

    Enhance infolist views by updating layout and adding company and contact fields with improved URL handling

--- app/Filament/App/Resources/OpportunityResource/Pages/ViewOpportunity.php
+++ app/Filament/App/Resources/OpportunityResource/Pages/ViewOpportunity.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace App\Filament\App\Resources\OpportunityResource\Pages;
 
+use App\Filament\App\Resources\CompanyResource;
 use App\Filament\App\Resources\OpportunityResource;
+use App\Filament\App\Resources\PeopleResource;
 use Filament\Actions;
 use Filament\Infolists;
 use Filament\Infolists\Components;
+use Filament\Infolists\Components\Split;
 use Filament\Infolists\Infolist;
 use Filament\Resources\Pages\ViewRecord;
 use Relaticle\CustomFields\Filament\Infolists\CustomFieldsInfolists;
@@ -31,10 +34,20 @@ final class ViewOpportunity extends ViewRecord
         return $infolist
             ->schema([
                 Components\Section::make()->schema([
-                    Infolists\Components\TextEntry::make('name'),
+                    Split::make([
+                        Infolists\Components\TextEntry::make('name')->grow(true),
+                        Infolists\Components\TextEntry::make('company.name')
+                            ->label('Company')
+                            ->color('primary')
+                            ->url(fn($record) => $record->company? CompanyResource::getUrl('view', [$record->company]) : null)
+                            ->grow(false),
+                        Infolists\Components\TextEntry::make('contact.name')
+                            ->label('Point of Contact')
+                            ->color('primary')
+                            ->url(fn($record) => $record->contact ? PeopleResource::getUrl('view', [$record->contact]) : null)
+                            ->grow(false),
+                    ]),
                     CustomFieldsInfolists::make()->columnSpanFull(),
-                    Infolists\Components\TextEntry::make('company.name')->label('Company'),
-                    Infolists\Components\TextEntry::make('contact.name')->label('Point of Contact'),
                 ]),
             ]);
     }
