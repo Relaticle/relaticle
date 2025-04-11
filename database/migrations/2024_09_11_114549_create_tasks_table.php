c49e2c1cdb37ad5216faec286a46227ceea93fff
manukminasyan <manuk.minasyan1@gmail.com>
2025-04-12 01:23:43 +0400

    Remove soft deletes

--- database/migrations/2024_09_11_114549_create_tasks_table.php
+++ database/migrations/2024_09_11_114549_create_tasks_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
             $table->unsignedBigInteger('order_column')->nullable();
 
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
