721332a7e899fdd97c581aaaf24940cc8ad65e51
manukminasyan <manuk.minasyan1@gmail.com>
2025-04-13 23:18:34 +0400

    Add sitemap generation command and configuration; update dependencies

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Sitemap\SitemapGenerator;
+
+final class GenerateSitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:generate-sitemap';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate the sitemap';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        SitemapGenerator::create(config('app.url'))
+            ->writeToFile(public_path('sitemap.xml'));
+    }
+}
