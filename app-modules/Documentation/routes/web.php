384a29a7d9193c01b1a9e317036af21a6d6a198b
manukminasyan <manuk.minasyan1@gmail.com>
2025-05-02 16:35:24 +0400

    Refactor code for improved readability and consistency, including type hinting and spacing adjustments

--- app-modules/Documentation/routes/web.php
+++ app-modules/Documentation/routes/web.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use Relaticle\Documentation\Http\Controllers\DocumentationController;
 
-Route::prefix('documentation')->name('documentation.')->group(function () {
+Route::prefix('documentation')->name('documentation.')->group(function (): void {
     Route::get('/', [DocumentationController::class, 'index'])->name('index');
     Route::get('/{type}', [DocumentationController::class, 'show'])->name('show');
 });
