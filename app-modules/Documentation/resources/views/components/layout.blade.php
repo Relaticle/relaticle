2123b47f3d2af63f49c86a293a2c130a7fd66c55
manukminasyan <manuk.minasyan1@gmail.com>
2025-05-03 01:38:42 +0400

    Refactor documentation layout and enhance search form styling for improved user experience

--- app-modules/Documentation/resources/views/components/layout.blade.php
+++ app-modules/Documentation/resources/views/components/layout.blade.php
@@ -6,8 +6,8 @@
     <div class="py-16 md:py-24 bg-white dark:bg-black relative">
         <!-- Subtle background elements -->
         <div class="absolute inset-0 bg-grid-pattern opacity-[0.01] dark:opacity-[0.02]"></div>
-        <div class="absolute -top-24 -left-24 w-96 h-96 bg-primary/5 dark:bg-primary/10 rounded-full blur-3xl"></div>
-        <div class="absolute -bottom-24 -right-24 w-96 h-96 bg-primary/5 dark:bg-primary/10 rounded-full blur-3xl"></div>
+        <div class="absolute top-24 left-24 w-36 h-36 md:w-96 md:h-96 bg-primary/5 dark:bg-primary/10 rounded-full blur-3xl"></div>
+        <div class="absolute -bottom-24 right-24 w-36 h-36 md:w-96 md:h-96 bg-primary/5 dark:bg-primary/10 rounded-full blur-3xl"></div>
 
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative">
             <!-- Main Documentation Container -->
