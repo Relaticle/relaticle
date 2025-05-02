9deacaf45fafbb331723faed866eaf9f3f844b17
manukminasyan <manuk.minasyan1@gmail.com>
2025-05-02 20:35:52 +0400

    Add Blade Icons configuration and update documentation card component to support dynamic icons

--- app-modules/Documentation/resources/views/components/card.blade.php
+++ app-modules/Documentation/resources/views/components/card.blade.php
@@ -8,19 +8,15 @@
 <div {{ $attributes->merge(['class' => 'group bg-white dark:bg-gray-900 rounded-xl border border-gray-100 dark:border-gray-800 hover:border-gray-200 dark:hover:border-gray-700 transition-all duration-300 hover:shadow-sm transform hover:-translate-y-1']) }}>
     <a href="{{ $link }}" class="block p-6">
         <div class="space-y-4">
-            @if($icon)
-                <div class="w-10 h-10 flex items-center justify-center rounded-lg bg-gray-50 dark:bg-gray-800 text-primary dark:text-primary-400 group-hover:bg-primary/10 dark:group-hover:bg-primary/20 transition-colors duration-300">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        {!! $icon !!}
-                    </svg>
-                </div>
-            @else
-                <div class="w-10 h-10 flex items-center justify-center rounded-lg bg-gray-50 dark:bg-gray-800 text-primary dark:text-primary-400 group-hover:bg-primary/10 dark:group-hover:bg-primary/20 transition-colors duration-300">
+            <div class="w-10 h-10 flex items-center justify-center rounded-lg bg-gray-50 dark:bg-gray-800 text-primary dark:text-primary-400 group-hover:bg-primary/10 dark:group-hover:bg-primary/20 transition-colors duration-300">
+                @if($icon)
+                    <x-dynamic-component :component="$icon" class="h-5 w-5" />
+                @else
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"/>
                     </svg>
-                </div>
-            @endif
+                @endif
+            </div>
             
             <div>
                 <h3 class="text-lg font-medium text-black dark:text-white group-hover:text-primary dark:group-hover:text-primary-400 transition-colors duration-300">
