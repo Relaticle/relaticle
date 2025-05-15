1f99ec6bbf64d553948c01fbc67c30910e4e5e23
manukminasyan <manuk.minasyan1@gmail.com>
2025-05-15 17:46:26 +0400

    Refactor Callback and Redirect controllers: implement stateless handling for Socialite driver

--- app/Http/Controllers/Auth/RedirectController.php
+++ app/Http/Controllers/Auth/RedirectController.php
@@ -6,12 +6,16 @@ namespace App\Http\Controllers\Auth;
 
 use App\Enums\SocialiteProvider;
 use Laravel\Socialite\Facades\Socialite;
+use Laravel\Socialite\Two\AbstractProvider;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 final readonly class RedirectController
 {
     public function __invoke(SocialiteProvider $provider): RedirectResponse
     {
-        return Socialite::driver($provider->value)->redirect();
+        /** @var AbstractProvider $driver */
+        $driver = Socialite::driver($provider->value);
+
+        return $driver->stateless()->redirect();
     }
 }
