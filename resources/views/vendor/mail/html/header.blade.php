7aa75b3f8c73d1404eea6bda6da34db4ea3c00de
manukminasyan <manuk.minasyan1@gmail.com>
2025-05-17 23:34:01 +0400

    Add email template components: create button, header, footer, layout, message, panel, subcopy, and table blade files

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            <img src="{{ asset('images/relaticle-logo.svg') }}" class="logo" alt="Relaticle Logo">
+        </a>
+    </td>
+</tr>
