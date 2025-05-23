88c56a993d1a8bebf3644540bc41241e50fe9dfe
manukminasyan <manuk.minasyan1@gmail.com>
2025-05-23 22:13:04 +0400

    Add type hint for input parameter in CreatesNewSocialUsers interface for improved type safety

--- app/Contracts/User/CreatesNewSocialUsers.php
+++ app/Contracts/User/CreatesNewSocialUsers.php
@@ -8,5 +8,10 @@ use App\Models\User;
 
 interface CreatesNewSocialUsers
 {
+    /**
+     * Create a new social user.
+     *
+     * @param  array<string, string>  $input
+     */
     public function create(array $input): User;
 }
