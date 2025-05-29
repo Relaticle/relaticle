3c920b802576409b95b4104e507322cdeaa59923
manukminasyan <manuk.minasyan1@gmail.com>
2025-05-29 16:50:43 +0400

    Update dependencies in composer.lock and add type annotations for improved type safety in TeamScope

--- app/Models/Scopes/TeamScope.php
+++ app/Models/Scopes/TeamScope.php
@@ -8,10 +8,15 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
 
+/**
+ * @template TModel of Model
+ */
 final class TeamScope implements Scope
 {
     /**
      * Apply the scope to a given Eloquent query builder.
+     *
+     * @param  Builder<TModel>  $builder
      */
     public function apply(Builder $builder, Model $model): void
     {
