1d230628c1a506c750b94b313765c8d56768b963
manukminasyan <manuk.minasyan1@gmail.com>
2025-06-28 20:02:28 +0400

    Add URL macros for app and public URLs; update registration form to include terms of service link

--- app/Filament/App/Pages/Auth/Register.php
+++ app/Filament/App/Pages/Auth/Register.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace App\Filament\App\Pages\Auth;
 
 use Filament\Forms\Components\Component;
+use Filament\Forms\Components\Placeholder;
 use Filament\Forms\Components\TextInput;
 use Filament\Pages\Auth\Register as BaseRegister;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\HtmlString;
 
 final class Register extends BaseRegister
 {
@@ -19,5 +22,23 @@ final class Register extends BaseRegister
             ->required()
             ->maxLength(255)
             ->unique($this->getUserModel());
+    }
+
+    protected function getForms(): array
+    {
+        return [
+            'form' => $this->form(
+                $this->makeForm()
+                    ->schema([
+                        $this->getNameFormComponent(),
+                        $this->getEmailFormComponent(),
+                        $this->getPasswordFormComponent(),
+                        $this->getPasswordConfirmationFormComponent(),
+                        Placeholder::make('terms')
+                            ->label(new HtmlString('By registering, you agree to our <a href="'.URL::getPublicUrl('terms-of-service').'" target="_blank" class="text-primary-600 hover:text-primary-500">Terms of Service</a>.')),
+                    ])
+                    ->statePath('data'),
+            ),
+        ];
     }
 }
