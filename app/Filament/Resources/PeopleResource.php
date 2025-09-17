2fe5f8c407b8e7eecd1a489a89553af20108c004
manukminasyan <manuk.minasyan1@gmail.com>
2025-09-17 16:03:03 +0400

    Refactor schema definition in PeopleResource for improved readability

--- app/Filament/Resources/PeopleResource.php
+++ app/Filament/Resources/PeopleResource.php
@@ -30,7 +30,6 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Components\Grid;
 use Filament\Schemas\Components\Utilities\Set;
 use Filament\Schemas\Schema;
-use Filament\Support\Enums\Size;
 use Filament\Support\Enums\Width;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
@@ -71,19 +70,17 @@ final class PeopleResource extends Resource
                             ->suffixAction(
                                 Action::make('Create Company')
                                     ->model(Company::class)
-                                    ->schema(function (Schema $schema) {
-                                        return $schema->components([
-                                            TextInput::make('name')
-                                                ->required(),
-                                            Select::make('account_owner_id')
-                                                ->model(Company::class)
-                                                ->relationship('accountOwner', 'name')
-                                                ->label('Account Owner')
-                                                ->preload()
-                                                ->searchable(),
-                                            CustomFields::form()->forSchema($schema)->build()->columns(1),
-                                        ]);
-                                    })
+                                    ->schema(fn (Schema $schema): \Filament\Schemas\Schema => $schema->components([
+                                        TextInput::make('name')
+                                            ->required(),
+                                        Select::make('account_owner_id')
+                                            ->model(Company::class)
+                                            ->relationship('accountOwner', 'name')
+                                            ->label('Account Owner')
+                                            ->preload()
+                                            ->searchable(),
+                                        CustomFields::form()->forSchema($schema)->build()->columns(1),
+                                    ]))
                                     ->modalWidth(Width::Large)
                                     ->slideOver()
                                     ->icon('heroicon-o-plus')
