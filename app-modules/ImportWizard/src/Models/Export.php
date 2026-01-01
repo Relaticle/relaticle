74f53c1fc5ebd770bb8b8124a8cd555b945d0bd0
manukminasyan <manuk.minasyan1@gmail.com>
2026-01-01 22:16:06 +0400

    refactor: remove redundant booted methods from Import and Export models

--- app-modules/ImportWizard/src/Models/Export.php
+++ app-modules/ImportWizard/src/Models/Export.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Relaticle\ImportWizard\Models;
 
 use App\Models\Concerns\HasTeam;
-use App\Models\User;
 use Filament\Actions\Exports\Models\Export as BaseExport;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 
@@ -13,18 +12,4 @@ final class Export extends BaseExport
 {
     use HasTeam;
     use HasUlids;
-
-    /**
-     * Bootstrap the model and its traits.
-     */
-    protected static function booted(): void
-    {
-        self::creating(function (Export $export): void {
-            if (auth()->check()) {
-                /** @var User $user */
-                $user = auth()->user();
-                $export->team_id = $user->currentTeam?->getKey();
-            }
-        });
-    }
 }
