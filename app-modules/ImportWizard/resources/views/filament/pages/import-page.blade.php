24971a470252c8b6438c6c0f114a41488ca8c8e0
manukminasyan <manuk.minasyan1@gmail.com>
2026-01-22 20:46:17 +0400

    Remove old import-wizard module

--- app-modules/ImportWizard/resources/views/filament/pages/import-page.blade.php
+++
@@ -1,6 +0,0 @@
-<x-filament-panels::page>
-    <livewire:import-wizard
-        :entity-type="static::getEntityType()"
-        :return-url="$this->getReturnUrl()"
-    />
-</x-filament-panels::page>
