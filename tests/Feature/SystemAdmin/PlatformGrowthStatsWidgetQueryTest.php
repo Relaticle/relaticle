10b28e81c9c1f06825db16f51dbdce19e3c7e954
manukminasyan <manuk.minasyan1@gmail.com>
2026-02-17 00:27:04 +0400

    test: add query count reproduction for PlatformGrowthStatsWidget

---
+++ tests/Feature/SystemAdmin/PlatformGrowthStatsWidgetQueryTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+use Filament\Facades\Filament;
+use Relaticle\SystemAdmin\Filament\Widgets\PlatformGrowthStatsWidget;
+use Relaticle\SystemAdmin\Models\SystemAdministrator;
+use Tests\Helpers\QueryCounter;
+
+beforeEach(function () {
+    $this->admin = SystemAdministrator::factory()->create();
+    $this->actingAs($this->admin, 'sysadmin');
+    Filament::setCurrentPanel('sysadmin');
+});
+
+it('tracks query count for PlatformGrowthStatsWidget', function () {
+    $counter = new QueryCounter();
+    $counter->start();
+
+    livewire(PlatformGrowthStatsWidget::class, ['pageFilters' => ['period' => '30']])
+        ->assertOk();
+
+    $counter->stop();
+
+    $counter->dump();
+
+    $totalQueries = $counter->count();
+
+    $companiesCount = $counter->findRepeated('from "companies" where "creation_source"');
+    $peopleCount = $counter->findRepeated('from "people" where "creation_source"');
+
+    dump("Companies count queries: {$companiesCount['count']}");
+    dump("People count queries: {$peopleCount['count']}");
+    dump("Total queries: {$totalQueries}");
+
+    expect($totalQueries)->toBeGreaterThan(0);
+});
