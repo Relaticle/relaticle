136982a2d450f4fdcf38c8637cb4d9648b0cdc1e
manukminasyan <manuk.minasyan1@gmail.com>
2026-02-16 07:52:24 +0400

    feat: schedule health check commands when enabled

--- routes/console.php
+++ routes/console.php
@@ -2,13 +2,13 @@
 
 declare(strict_types=1);
 
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
+use Illuminate\Support\Facades\Schedule;
+use Spatie\Health\Commands\DispatchQueueCheckJobsCommand;
+use Spatie\Health\Commands\RunHealthChecksCommand;
+use Spatie\Health\Commands\ScheduleCheckHeartbeatCommand;
+
+if (config('app.health_checks_enabled')) {
+    Schedule::command(RunHealthChecksCommand::class)->everyMinute();
+    Schedule::command(DispatchQueueCheckJobsCommand::class)->everyMinute();
+    Schedule::command(ScheduleCheckHeartbeatCommand::class)->everyMinute();
+}
