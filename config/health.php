e2c697190a00b99e7e0e74fde04f8a3232a76aa5
manukminasyan <manuk.minasyan1@gmail.com>
2026-02-16 07:48:45 +0400

    chore: configure health checks for in-memory store and oh dear

--- config/health.php
+++ config/health.php
@@ -7,24 +7,7 @@ return [
      * can use multiple stores at the same time.
      */
     'result_stores' => [
-        Spatie\Health\ResultStores\EloquentHealthResultStore::class => [
-            'connection' => env('HEALTH_DB_CONNECTION', env('DB_CONNECTION')),
-            'model' => Spatie\Health\Models\HealthCheckResultHistoryItem::class,
-            'keep_history_for_days' => 5,
-        ],
-
-        /*
-        Spatie\Health\ResultStores\CacheHealthResultStore::class => [
-            'store' => 'file',
-        ],
-
-        Spatie\Health\ResultStores\JsonFileHealthResultStore::class => [
-            'disk' => 's3',
-            'path' => 'health.json',
-        ],
-
         Spatie\Health\ResultStores\InMemoryHealthResultStore::class,
-        */
     ],
 
     /*
@@ -32,58 +15,7 @@ return [
      * For Slack you need to install laravel/slack-notification-channel.
      */
     'notifications' => [
-        /*
-         * Notifications will only get sent if this option is set to `true`.
-         */
-        'enabled' => true,
-
-        'notifications' => [
-            Spatie\Health\Notifications\CheckFailedNotification::class => ['mail'],
-        ],
-
-        /*
-         * Here you can specify the notifiable to which the notifications should be sent. The default
-         * notifiable will use the variables specified in this config file.
-         */
-        'notifiable' => Spatie\Health\Notifications\Notifiable::class,
-
-        /*
-         * When checks start failing, you could potentially end up getting
-         * a notification every minute.
-         *
-         * With this setting, notifications are throttled. By default, you'll
-         * only get one notification per hour.
-         */
-        'throttle_notifications_for_minutes' => 60,
-        'throttle_notifications_key' => 'health:latestNotificationSentAt:',
-
-        /*
-         * When set to true, notifications will only be sent when at least one
-         * check has a 'failed' status. Warnings will be ignored.
-         */
-        'only_on_failure' => false,
-
-        'mail' => [
-            'to' => 'your@example.com',
-
-            'from' => [
-                'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-                'name' => env('MAIL_FROM_NAME', 'Example'),
-            ],
-        ],
-
-        'slack' => [
-            'webhook_url' => env('HEALTH_SLACK_WEBHOOK_URL', ''),
-
-            /*
-             * If this is set to null the default channel of the webhook will be used.
-             */
-            'channel' => null,
-
-            'username' => null,
-
-            'icon' => null,
-        ],
+        'enabled' => false,
     ],
 
     /*
@@ -92,23 +24,10 @@ return [
      * Oh Dear, you can also have access to more advanced notification options.
      */
     'oh_dear_endpoint' => [
-        'enabled' => false,
-
-        /*
-         * When this option is enabled, the checks will run before sending a response.
-         * Otherwise, we'll send the results from the last time the checks have run.
-         */
+        'enabled' => env('OH_DEAR_HEALTH_CHECK_SECRET') !== null && env('OH_DEAR_HEALTH_CHECK_SECRET') !== '',
         'always_send_fresh_results' => true,
-
-        /*
-         * The secret that is displayed at the Application Health settings at Oh Dear.
-         */
-        'secret' => env('OH_DEAR_HEALTH_CHECK_SECRET'),
-
-        /*
-         * The URL that should be configured in the Application health settings at Oh Dear.
-         */
-        'url' => '/oh-dear-health-check-results',
+        'secret' => env('OH_DEAR_HEALTH_CHECK_SECRET', ''),
+        'url' => 'oh-dear-health-check-results',
     ],
 
     /*
