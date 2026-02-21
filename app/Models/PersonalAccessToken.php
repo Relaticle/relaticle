083dd20baf127c4b9c64c671cda8388a4d261eb4
manukminasyan <manuk.minasyan1@gmail.com>
2026-02-21 07:45:40 +0400

    fix: remove token from PersonalAccessToken fillable and fix imports

--- app/Models/PersonalAccessToken.php
+++ app/Models/PersonalAccessToken.php
@@ -4,16 +4,17 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Laravel\Sanctum\PersonalAccessToken as SanctumPersonalAccessToken;
 
 class PersonalAccessToken extends SanctumPersonalAccessToken
 {
-    use \Illuminate\Database\Eloquent\Factories\HasFactory;
+    use HasFactory;
+
     /** @var array<int, string> */
     protected $fillable = [
         'name',
-        'token',
         'abilities',
         'expires_at',
         'team_id',
