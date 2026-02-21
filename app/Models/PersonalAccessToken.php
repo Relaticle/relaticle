61a864621d401b1580f6caa535a10c70a10ea3f5
manukminasyan <manuk.minasyan1@gmail.com>
2026-02-21 08:02:58 +0400

    fix: remove unused HasFactory trait from PersonalAccessToken

--- app/Models/PersonalAccessToken.php
+++ app/Models/PersonalAccessToken.php
@@ -4,14 +4,11 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Laravel\Sanctum\PersonalAccessToken as SanctumPersonalAccessToken;
 
 class PersonalAccessToken extends SanctumPersonalAccessToken
 {
-    use HasFactory;
-
     /** @var array<int, string> */
     protected $fillable = [
         'name',
