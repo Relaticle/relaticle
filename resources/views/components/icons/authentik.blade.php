9e375ffb888e28ae80b80e672eb9f1009d3c3261
manukminasyan <manuk.minasyan1@gmail.com>
2026-03-16 19:20:19 +0400

    feat: add SVG icon components for all social login providers

---
+++ resources/views/components/icons/authentik.blade.php
@@ -0,0 +1,3 @@
+<svg {{ $attributes }} viewBox="0 0 24 24" fill="currentColor">
+    <path d="M12 2L2 7v10l10 5 10-5V7L12 2zm0 2.18L19.82 8 12 11.82 4.18 8 12 4.18zM4 9.18l7 3.5v7.14l-7-3.5V9.18zm16 0v7.14l-7 3.5v-7.14l7-3.5z"/>
+</svg>
---
+++ resources/views/components/icons/azure.blade.php
@@ -0,0 +1,3 @@
+<svg {{ $attributes }} viewBox="0 0 24 24" fill="currentColor">
+    <path d="M13.05 4.24L7.56 18.05l-2.8-3.24L0 22.42h18.67L13.05 4.24zM14.28 1.58L10.11 14.17l6.72 7.6h6.44L14.28 1.58z"/>
+</svg>
