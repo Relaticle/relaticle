cb5e39e57fa14998240c75bcd3d12181f849f053
Manuk <manuk.minasyan1@gmail.com>
2026-04-13 12:21:12 +0400

    perf(tests): add global test safeguards in TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,8 +5,19 @@ declare(strict_types=1);
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Exceptions;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Sleep;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Http::preventStrayRequests();
+        Sleep::fake(syncWithCarbon: true);
+        Exceptions::fake();
+        $this->withoutVite();
+    }
 }
