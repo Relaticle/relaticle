491368af14f42f4901aae0b7096de55108170a0e
Manuk <manuk.minasyan1@gmail.com>
2026-05-07 21:44:14 +0400

    fix: implement queue ShouldBeUnique contract so unique-job lock fires

--- app/Jobs/FetchFaviconForCompany.php
+++ app/Jobs/FetchFaviconForCompany.php
@@ -9,7 +9,7 @@ use App\Exceptions\SsrfGuardException;
 use App\Models\Company;
 use App\Services\Favicon\SsrfGuard;
 use AshAllenDesign\FaviconFetcher\Facades\Favicon;
-use Illuminate\Contracts\Broadcasting\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Foundation\Queue\Queueable;
