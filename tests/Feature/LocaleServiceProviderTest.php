ad160e404887638e8f5e35eafd9804f456957ba4
Manuk <manuk.minasyan1@gmail.com>
2026-05-09 01:32:23 +0400

    feat: add LocaleServiceProvider wiring Carbon and Number to app locale

---
+++ tests/Feature/LocaleServiceProviderTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Providers\LocaleServiceProvider;
+use Carbon\Carbon;
+use Illuminate\Support\Number;
+
+it('localizes Carbon dates when app locale is set', function (): void {
+    app()->setLocale('fr');
+    (new LocaleServiceProvider(app()))->boot();
+
+    $formatted = Carbon::parse('2026-05-09')->translatedFormat('F');
+
+    expect($formatted)->toBe('mai');
+});
+
+it('localizes Number formatting when app locale is set', function (): void {
+    app()->setLocale('fr');
+    (new LocaleServiceProvider(app()))->boot();
+
+    expect(Number::format(1234.5))->toBe("1\u{202F}234,5");
+});
