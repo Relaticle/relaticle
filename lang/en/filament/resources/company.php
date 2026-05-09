04898e1ed8a7f4fbb759ed198f33533e33520c24
Manuk <manuk.minasyan1@gmail.com>
2026-05-09 04:37:13 +0400

    feat: add CompanyResource translation keys

---
+++ lang/en/filament/resources/company.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'label' => 'Company',
+    'plural_label' => 'Companies',
+    'navigation_label' => 'Companies',
+    'navigation_group' => 'Workspace',
+
+    'fields' => [
+        'name' => [
+            'label' => 'Company',
+        ],
+        'account_owner' => [
+            'label' => 'Account Owner',
+        ],
+        'account_owner_id' => [
+            'label' => 'Account Owner',
+        ],
+        'created_by' => [
+            'label' => 'Created By',
+        ],
+        'creator' => [
+            'label' => 'Created By',
+        ],
+        'creation_source' => [
+            'label' => 'Creation Source',
+        ],
+        'created_at' => [
+            'label' => 'Creation Date',
+        ],
+        'updated_at' => [
+            'label' => 'Last Update',
+        ],
+        'deleted_at' => [
+            'label' => 'Deleted At',
+        ],
+    ],
+];
