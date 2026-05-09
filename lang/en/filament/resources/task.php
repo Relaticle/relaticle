a98d34663ff863cc9c93f76bdecca817219c3599
Manuk <manuk.minasyan1@gmail.com>
2026-05-09 05:43:46 +0400

    feat: i18n TaskResource
    Refactor TaskResource and its subtree (TaskForm, ManageTasks) to use
    translation keys under filament/resources/task. Add lang/en/filament/resources/task.php,
    wrap all hardcoded labels/filters/action labels in __(), set static navigation
    properties to null with method overrides. Remove 5 baseline entries.

---
+++ lang/en/filament/resources/task.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'label' => 'Task',
+    'plural_label' => 'Tasks',
+    'navigation_label' => 'Tasks',
+
+    'fields' => [
+        'assignees' => [
+            'label' => 'Assignees',
+        ],
+        'companies' => [
+            'label' => 'Companies',
+        ],
+        'people' => [
+            'label' => 'People',
+        ],
+        'creator' => [
+            'label' => 'Created By',
+        ],
+        'created_at' => [
+            'label' => 'Created At',
+        ],
+        'updated_at' => [
+            'label' => 'Updated At',
+        ],
+        'deleted_at' => [
+            'label' => 'Deleted At',
+        ],
+    ],
+
+    'filters' => [
+        'assigned_to_me' => [
+            'label' => 'Assigned to me',
+        ],
+        'creation_source' => [
+            'label' => 'Creation Source',
+        ],
+    ],
+
+    'pages' => [
+        'list' => [
+            'actions' => [
+                'import' => [
+                    'label' => 'Import tasks',
+                ],
+                'import_export' => [
+                    'label' => 'Import / Export',
+                ],
+            ],
+        ],
+    ],
+];
